476b187a93b27421d16ab3dbbcb717606e09ecf5
Mat <mohamadalialtellawi@gmail.com>
2022-05-29 08:35:05 +0300

    add command to change file revision and tested ok

--- buisness.py
+++ buisness.py
@@ -1,1 +1,42 @@
+from pathlib import Path
+import shutil
 
+from pprint import pprint
+
+def get_files_of_extension(directory:Path, extension:str) -> Path:
+    ext = f'*.{extension}'
+    p = directory.glob(ext)
+    files = [x for x in p if x.is_file()]
+    return files
+
+def change_revision(directory:Path,extension:str, revision:str, length:int = 2):
+    files = get_files_of_extension(directory,extension)
+    for file in files:
+        old_name = file.stem
+        new_name = old_name[0:-length]
+        new_name += revision
+        backup_dir = directory / 'backup'
+
+        backup_dir.mkdir(exist_ok=True)
+        shutil.copy(file,backup_dir)
+
+        new_file = file.with_stem(new_name)
+        file.replace(new_file)
+    print ('\nDone!')
+
+def main():
+    test_path = Path.cwd()
+    # Search the directory
+    p = list(test_path.glob('*.*'))
+    pprint(p)
+    # Search directories and subdirectories, recursively
+    p = list(test_path.rglob('*.*'))
+    pprint(p)
+
+    pprint("="*20)
+    files = [x for x in p if x.is_file()]
+    pprint(files)
+
+if __name__ == "__main__":
+    main()
+    #change_revision(Path(r'C:\Users\mali\Downloads\DTV\DTV'),'dwg','0004',length=2)
--- main.py
+++ main.py
@@ -1,11 +1,73 @@
 import db
+import pathlib
+import buisness
+
+def show_title():
+    print("The Mini File Manager Program")
+    print()
 
 
-for f in db.get_temp_files():
-    print(f)
+
+def show_menu():
+    print("COMMAND MENU")
+    print("dir - Assign Working Directory")
+    print("rev - Change File Revision")
+    print("x = Exit Program")
+
+def get_int(prompt, maxNum = 1000):
+    while True:
+        try:
+            number = int(input(prompt))
+        except ValueError:
+            print("Invalid whole number (integer). please try again./n")
+            continue
+        if number < 1 or number > maxNum:
+            print("That number is out of range. Please try again.\n")
+        else:
+            return number
 
 
+def change_working_directory():
+    while True:
+        try:
+            new_path = pathlib.Path( input("Select New Directory to work inside:\t"))
+        except ValueError:
+            print("Invalid Path . please try again./n")
+            continue
+        if not new_path.is_dir():
+            print("That Path is not correct. Please try again.\n")
+        else:
+            return new_path.resolve()
+
+def change_file_revision(directory):
+    try:
+        file_extension = input("what is the files extension to change the revision? (dwg\\pdf)\n")
+        new_revision = input("enter the new revision:\n")
+        buisness.change_revision(directory,file_extension,new_revision,len(new_revision))
+    except:
+        print("\n!Error in change_file_revision")
 
 
+def main():
+    show_title()
+    show_menu()
 
-print("\nDone!")
+    wdir = pathlib.Path().cwd().resolve()
+
+
+    while True:
+        print(f'Current Working Directory is\t{wdir}')
+        command = input("Command: ").lower()
+        if command == "dir":
+            wdir = change_working_directory()
+        elif command == "rev":
+            change_file_revision(wdir)
+
+        elif command == "x":
+            print("Bye!")
+            break
+        else:
+            print("Not Valid Command. Please try again.\n")
+
+if __name__ == "__main__":
+    main()
